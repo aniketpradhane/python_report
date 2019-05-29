b6e733123ae3300eac117b598bcc2e50a83a1070
root <ani.pradhan27@gmail.com>
2019-05-29 07:57:21 +0000

    New1

--- read_report_new.py
+++ read_report_new.py
@@ -34,7 +34,7 @@ df6 = df6[df6['IP Address'].notnull()]
 df4 = df4[df4['Resource Name'].notnull()]
 
 #combine sheets with formating
-con = pd.concat([df4,df1.reset_index(name='J'),df2.reset_index(drop=True),df3.reset_index(drop=True),df5.reset_index(drop=True),df6.reset_index(drop=True)],axis = 1, keys = ['','CPU','Memory','Disk','Network_OUT','Network_IN'],join='inner')
+con = pd.concat([df4,df1.reset_index(name='IP Address'),df2.reset_index(drop=True),df3.reset_index(drop=True),df5.reset_index(drop=True),df6.reset_index(drop=True)],axis = 1, keys = ['','CPU','Memory','Disk','Network_OUT','Network_IN'],join='inner')
 
 ht = con.to_html('ht.html')
 
